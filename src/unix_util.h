c14343c1e6f12beab970cd3af7b0f40b9b889b1d
MierenManz <christiaanmens@gmail.com>
2022-12-20 22:02:30 +0100

    add a lot of things
    Co-authored-by: Dj <DjDeveloperr@users.noreply.github.com>

---
+++ src/unix_util.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "errno.h"
+#include "wasi.h"
+
+#define ERR(x) case x: return WASI_##x;
+
+#define SYSCALL(result) \
+  if (result == -1) { \
+    switch (errno) { \
+      ERR(E2BIG) \
+      ERR(EACCES) \
+      ERR(EADDRINUSE) \
+      ERR(EADDRNOTAVAIL) \
+      ERR(EAFNOSUPPORT) \
+      ERR(EAGAIN) \
+      ERR(EALREADY) \
+      ERR(EBADF) \
+      ERR(EBADMSG) \
+      ERR(EBUSY) \
+      ERR(ECANCELED) \
+      ERR(ECHILD) \
+      ERR(ECONNABORTED) \
+      ERR(ECONNREFUSED) \
+      ERR(ECONNRESET) \
+      ERR(EDEADLK) \
+      ERR(EDESTADDRREQ) \
+      ERR(EDOM) \
+      ERR(EDQUOT) \
+      ERR(EEXIST) \
+      ERR(EFAULT) \
+      ERR(EFBIG) \
+      ERR(EHOSTUNREACH) \
+      ERR(EIDRM) \
+      ERR(EILSEQ) \
+      ERR(EINPROGRESS) \
+      ERR(EINTR) \
+      ERR(EINVAL) \
+      ERR(EIO) \
+      ERR(EISCONN) \
+      ERR(EISDIR) \
+      ERR(ELOOP) \
+      ERR(EMFILE) \
+      ERR(EMLINK) \
+      ERR(EMSGSIZE) \
+      ERR(EMULTIHOP) \
+      ERR(ENAMETOOLONG) \
+      ERR(ENETDOWN) \
+      ERR(ENETRESET) \
+      ERR(ENETUNREACH) \
+      ERR(ENFILE) \
+      ERR(ENOBUFS) \
+      ERR(ENODEV) \
+      ERR(ENOENT) \
+      ERR(ENOEXEC) \
+      ERR(ENOLCK) \
+      ERR(ENOLINK) \
+      ERR(ENOMEM) \
+      ERR(ENOMSG) \
+      ERR(ENOPROTOOPT) \
+      ERR(ENOSPC) \
+      ERR(ENOSYS) \
+      ERR(ENOTCONN) \
+      ERR(ENOTDIR) \
+      ERR(ENOTEMPTY) \
+      ERR(ENOTRECOVERABLE) \
+      ERR(ENOTSOCK) \
+      ERR(ENOTSUP) \
+      ERR(ENOTTY) \
+      ERR(ENXIO) \
+      ERR(EOVERFLOW) \
+      ERR(EOWNERDEAD) \
+      ERR(EPERM) \
+      ERR(EPIPE) \
+      ERR(EPROTO) \
+      ERR(EPROTONOSUPPORT) \
+      ERR(EPROTOTYPE) \
+      ERR(ERANGE) \
+      ERR(EROFS) \
+      ERR(ESPIPE) \
+      ERR(ESRCH) \
+      ERR(ESTALE) \
+      ERR(ETIMEDOUT) \
+      ERR(ETXTBSY) \
+      ERR(EXDEV) \
+    } \
+  }
--- src/wasi.ts
+++ src/wasi.ts
@@ -1,1 +1,40 @@
-const lib = Deno.dlopen()
+const {
+  wasi_clock_res_get,
+  wasi_time_get,
+} = Deno.dlopen(
+  new URL("../wasi.so", import.meta.url),
+  {
+    wasi_clock_res_get: {
+      parameters: ["u32", "buffer"],
+      result: "u32",
+    },
+
+    wasi_time_get: {
+      parameters: ["u32", "buffer"],
+      result: "u32",
+    },
+  } as const,
+).symbols;
+
+export function instanciateWasi(
+  bytes: Uint8Array,
+  imports: WebAssembly.Imports = {},
+): WebAssembly.WebAssemblyInstantiatedSource {
+  const module = new WebAssembly.Module(bytes);
+  const instance = new WebAssembly.Instance(module, {
+    wasi_snapshot_preview1: {
+      clock_get_res: (clockId: number, offset: number) => {
+        return wasi_clock_res_get(clockId, new Uint8Array((instance.exports.memory as WebAssembly.Memory).buffer, offset, 8));
+      },
+      clock_time_get: (clockId: number, _precision: bigint, offset: number) => {
+        return wasi_time_get(clockId, new Uint8Array((instance.exports.memory as WebAssembly.Memory).buffer, offset, 8));
+      },
+    },
+    ...imports,
+  });
+
+  return {
+    module,
+    instance,
+  };
+}
